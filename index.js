494f5e047161fcda4eb18b5944fc60310ba0752a
rachit2501 <rac.sri25@gmail.com>
2019-10-17 03:38:13 +0530

    mongodb and post req integrared. Need schema

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express =require("express");
 const app  = express();
-// const body = require('body-parser');
+const body = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use(express.static('public'));
-// app.use(body.json());
-// app.use(body.urlencoded());
+ app.use(body.json());
+ app.use(body.urlencoded());
+
+ mongoose.connect("mongodb://localhost/user",{newUrlParser:true})
+    .then(()=>console.log("Connected To mongodb"))
+    .catch(()=>console.log("Failed to connect to monogdb"));
+
+
 
 app.get('/', (req , res)=>
 {
     res.sendFile(__dirname+'/index.html');
 });
-    
+
+app.post('/submit' , (req , res)=>
+{
+    console.log(req.body);
+})
 app.listen(8080 , ()=>console.log("Listening on port 8080"));
 
